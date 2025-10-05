06b7ab8a96f530b1cb06a5a30029b1ab05d42ba1
vinils <vinils>
2025-10-05 10:59:50 -0300

    removing id from category entity

--- internal/entity/category.go
+++ internal/entity/category.go
@@ -6,7 +6,6 @@ import (
 )
 
 type Category struct {
-	ID        uint      `json:"id"`
 	Name      string    `json:"name"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
