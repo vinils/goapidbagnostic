8021e1252c75be9798cde8fc080f59b4cb60fe22
vinils <vinils>
2025-10-08 17:35:28 -0300

    adding create and list category routers

--- main.go
+++ main.go
@@ -6,6 +6,10 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/vinils/goapitemplate/internal/controller"
+	"github.com/vinils/goapitemplate/internal/repository"
+	"github.com/vinils/goapitemplate/internal/repository/inmemory"
+	"github.com/vinils/goapitemplate/internal/repository/postgre"
 )
 
 type timeStruct struct {
@@ -20,13 +24,39 @@ func getHealthCheck(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, newTimeNow())
 }
 
+func getRepository(cnnStr string) repository.IRepository {
+	if cnnStr == "" {
+		return inmemory.NewRepository()
+	}
+
+	ps, err := postgre.NewRepository(cnnStr)
+	if err != nil {
+		panic("Could not connect or invalid connection string" + err.Error())
+	}
+
+	if mgErr := ps.MigrateModels(); mgErr != nil {
+		panic("Could not migrate model " + mgErr.Error())
+	}
+
+	return ps
+}
+
 func setupRouter() *gin.Engine {
 	router := gin.Default()
-
+	cnnStr := os.Getenv("CNNSTR")
+	repo := getRepository(cnnStr)
+	ctrl := controller.NewCategory()
 	basePath := "/api/v1"
+
 	v1 := router.Group(basePath)
 	{
 		v1.GET("/healthcheck", getHealthCheck)
+		v1.POST("/category", func(ctx *gin.Context) {
+			ctrl.Create(ctx, repo.Category())
+		})
+		v1.GET("/categories", func(ctx *gin.Context) {
+			ctrl.List(ctx, repo.Category())
+		})
 	}
 	return router
 }
--- main_test.go
+++ main_test.go
@@ -26,21 +26,32 @@ func TestNewTimeNow(test *testing.T) {
 }
 
 func TestHealthCheck(t *testing.T) {
-	expectedStatus := http.StatusOK
 	router := setupRouter()
 
-	newRecord := httptest.NewRecorder()
-	req, err := http.NewRequest("GET", "/api/v1/healthcheck", nil)
-	assert.NoError(t, err)
+	req1, req1Err := http.NewRequest("GET", "/api/v1/healthcheck", nil)
+	req2, req2Err := http.NewRequest("GET", "/api/v1/categories", nil)
+	req3, req3Err := http.NewRequest("POST", "/api/v1/category", nil)
+
+	assert.NoError(t, req1Err)
+	assert.NoError(t, req2Err)
+	assert.NoError(t, req3Err)
 
 	expectedTime := time.Now()
-	router.ServeHTTP(newRecord, req)
+	newRecordReq1 := httptest.NewRecorder()
+	router.ServeHTTP(newRecordReq1, req1)
+	newRecordReq2 := httptest.NewRecorder()
+	router.ServeHTTP(newRecordReq2, req2)
+	newRecordReq3 := httptest.NewRecorder()
+	router.ServeHTTP(newRecordReq3, req3)
 
 	var actualTime timeStruct
-	err = json.Unmarshal(newRecord.Body.Bytes(), &actualTime)
+	req1Err = json.Unmarshal(newRecordReq1.Body.Bytes(), &actualTime)
 
-	assert.Equal(t, expectedStatus, newRecord.Code)
-	assert.Equal(t, "application/json; charset=utf-8", newRecord.Header().Get("Content-Type"))
-	assert.NoError(t, err)
+	assert.Equal(t, http.StatusOK, newRecordReq1.Code)
+	assert.Equal(t, "application/json; charset=utf-8", newRecordReq1.Header().Get("Content-Type"))
+	assert.NoError(t, req1Err)
 	assert.WithinDuration(t, expectedTime, actualTime.Time, time.Second)
+
+	assert.Equal(t, http.StatusOK, newRecordReq2.Code)
+	assert.Equal(t, http.StatusBadRequest, newRecordReq3.Code)
 }
